93d03eee8998ddd09eeb8c167a16d2a2222c0da3
Gary Smith <bk@kc.gs>
2020-12-08 13:27:35 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -24,9 +24,7 @@ fi
 # Decide what to do
 if  [ "$1" == "develop" ]
 then
-  # rm -rf $GATSBY_DIR/public
   gatsby develop --host 0.0.0.0
-
 elif  [ "$1" == "build" ]
 then
   rm -rf $GATSBY_DIR/public
@@ -42,7 +40,8 @@ elif  [ "$1" == "develop-no-cache" ]
 then
   rm -rf $GATSBY_DIR/public
   rm -rf $GATSBY_DIR/.cache
-  gatsby develop --host 0.0.0.0
+  gatsby develop --host 0.0.0.0 --verbose
+
 
 else
   exec $@
